136a1cf2bf4c01d5808a40503eb74f3a516fce19
Ana Marr <acarolinasim@gmail.com>
2020-04-27 15:50:38 +0930

    readme-components-prompts

--- index.js
+++ index.js
@@ -2,25 +2,43 @@ var inquirer = require("inquirer");
 var fs = require('fs');
 
 inquirer.prompt([
-  {
-
+    {
     name: "username",
     message: "Enter your GitHub username"
-  },
-  {
-   
-    message: "Enter your repository name:",
-    name: "reponame",
-   
-  },
-  {
+    },
+    {
     message: "Enter the title of your project: ",
     name: "title",
-  },
-  {
-      message: "Enter a brief description of your project:",
-      name: "description"
-  }
+    },
+    {
+    message: "A little info about your project:",
+    name: "description"
+    },
+    { 
+    message: "Explain how to run the automated tests for this system",
+    name: "testing"
+    },
+    {
+    message: "How would you install this project?",
+    name: "instalation"
+    },
+    {
+    type: 'checkbox',
+    name: 'license',
+    message: "Choose the project license:",
+    choices: [
+            "MIT", "ISC", "Other"
+        ],
+    initial: 1
+    },
+    {
+    message: "Give details of your code of conduct, and the process for submitting pull requests",
+    name: "contributing"
+    },
+    {
+    message:"Include step by step guide to use your project:",
+    name: "Usage"
+    }
 ]).then(function(data) {
 
   fs.writeFile("README.md", JSON.stringify(data, null, '\t'), function(err) {
